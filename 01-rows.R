7aada14bf3a5fac86036a6f084464bfe5594cb13
Kivanya Naidoo <kivanya.naidoo@derivco.co.za>
2022-03-15 08:55:41 +0000

    adding rows script

---
+++ 01-rows.R
@@ -0,0 +1,59 @@
+#=============================================================================
+# Tidyverse and data wrangling - dplyr: Operations on rows
+
+library(dplyr)
+library(nycflights13)
+
+# FILTERING --------------------------------------------------------------------
+
+# 1. Extract the flights which departed between 10 and 16 June 2013.
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
+
+# 2. Extract only the data for the UA carrier and do not include a distance of
+# > 1000miles
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
+
+
+# 4. Extract only the data for the UA carrier only for the 6th month and calculate
+# the average distance travelled
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
+
+# COUNT ------------------------------------------------------------------------
+
+# 2. How many flights departed from each of the origin airports? Sort in
+# descending order.
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
+
+# 3. How many flights departed from each of the origin airports broken down by month?
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
+
+# ARRANGE -----------------------------------------------------------------------
+
+# 4. Using flights find which carriers are associated with the top 5 departure
+# delays. dep_delay carrier
+#      1301 HA
+#      1137 MQ
+#      1126 MQ
+#      1014 AA
+#      1005 MQ
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
+
+
+# DISTINCT ----------------------------------------------------------------
+
+# 5. How many unique airlines are there in the data?
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
